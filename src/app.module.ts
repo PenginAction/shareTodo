9df13a87688ca880f0b6939f52842f53c3629998
PenginAction <udonfox15@gmail.com>
2023-10-31 14:09:10 +0900

    GraphQL is setup

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import * as process from 'process';
+import { join } from 'path';
+import { ProductModule } from './product/product.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: true,
+      autoSchemaFile: join(process.cwd(), 'src/schema.sql'),
+    }),
+    ProductModule,
+  ],
+})
+export class AppModule {}
