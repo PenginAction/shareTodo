a141eaf274e4a334fbdcad5794fc673eeee9308d
PenginAction <udonfox15@gmail.com>
2023-11-15 14:22:15 +0900

    correction

--- src/auth/guards/gql-auth-guard.ts
+++ src/auth/guards/gql-auth-guard.ts
@@ -2,7 +2,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { ExecutionContext } from '@nestjs/common';
 import { GqlExecutionContext } from '@nestjs/graphql';
 
-export class GqlAuthGuard extends AuthGuard(['local', 'google']) {
+export class GqlAuthGuard extends AuthGuard('local') {
   constructor() {
     super();
   }
